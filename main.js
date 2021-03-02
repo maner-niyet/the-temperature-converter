e45dce90ea8394012fc662f68fc72cb7f775c018
ManerNiyet <manernieyt15@gmail.com>
2021-03-02 15:46:03 -0500

    html and js added

---
+++ main.js
@@ -0,0 +1,11 @@
+/* C to F */
+var inputCelsius = 5;
+var outputFahrenheit = (inputCelsius * 9) / 5 + 32;
+var convert1 = inputCelsius + "\u00B0 Celsius is " + outputFahrenheit + "\u00B0 Fahrenheit";
+console.log(convert1);
+
+/* F to C */
+var inputFahrenheit = 41;
+var outputCelcius = (inputFahrenheit - 32) * 5 / 9;
+var convert2 = inputFahrenheit + "\u00B0 Fahrenheit is " + outputCelcius + "\u00B0 Celsius";
+console.log(convert2);
